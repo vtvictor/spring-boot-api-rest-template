5c032227d3cd725b7eccf7e3af70d1b27f661d5c
vtvictor <victorluiz.bsi@gmail.com>
2023-08-23 11:44:43 -0300

    first commit

---
+++ src/main/java/com/example/demoresttemplate/DemoRestTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoresttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoRestTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoRestTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoresttemplate/DemoRestTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoresttemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRestTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
